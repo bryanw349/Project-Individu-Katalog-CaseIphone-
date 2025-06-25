b5dcddfb345562df9a64997a0dd84ce88e7d8869
bryanw349 <bryan.wiranto@si.ukdw.ac.id>
2025-06-25 23:04:12 +0700

    Delete Lab6 directory

--- Lab6/hapus_dosen.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("sambungan.php");
-$niknya = $_GET["nik"];
-$sql = "DELETE FROM dosen WHERE nik='$niknya'";
-$conn->query(query: $sql);
-echo "<a href=\"hasil.php\">Lihat Daftar Dosen</a>";
-?>
--- Lab6/sambungan.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$conn = new mysqli("127.0.0.1", "root", "", "sia");
-
-if ($conn->connect_error) {
-    die("Terjadi kesalahan yang fatal: " . $conn->connect_error);
-}
-?>
